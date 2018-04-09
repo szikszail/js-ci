2262fc1222a4326d3aea40fad5f2915741b32ac1
Szikszai.Laszlo <Laszlo_Szikszai@epam.com>
2018-04-09 10:41:55 +0200

    Copied source from js-webinar

---
+++ module-4/toCamelCase.js
@@ -0,0 +1,8 @@
+'use strict';
+/**
+ * It returns the camel-case version of string.
+ * E.g.: simple lowercase string => SimpleLowercaseString
+ *
+ * @param {string} toConvert
+ * @returns {string} camel-case string or empty string in other cases
+ */
